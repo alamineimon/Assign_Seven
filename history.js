849d6ce2667d5a624d623ee0cd3ba1e9a426f52d
Ridoy Ahmed <ridoyahmed@gmail.com>
2022-09-15 09:15:55 +0600

    Display Card in 3 column fixed

--- history.js
+++ history.js
@@ -3,24 +3,20 @@ const histories = document.getElementById("histories");
 function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
-
   newRow.innerHTML = `
   <h3>${questionText}</h3>
-  <div>
   <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
   <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+  <div'>
   </div>
   `;
-
   histories.appendChild(newRow);
-
   let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
   previousTests.push({ questionText, timeTaken, errorCount });
   localStorage.setItem("testHistory", JSON.stringify(previousTests));
-
-  displayHistory();
 }
-addHistory()
+displayHistory();
+// addHistory()
 
 function displayHistory() {
   histories.innerHTML = "";
